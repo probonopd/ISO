ad6829e49e30b0cf60cad7eef8c82d46a2c1cb1a
pkgdemon <pkgdemonteam@gmail.com>
2024-08-11 19:10:47 -0500

    Remove old code not needed after refactors

--- build.sh
+++ build.sh
@@ -55,26 +55,12 @@ echo "git" > config/package-lists/gershwin.list.chroot
 
 # Modify package list extract GNUstep
 cp ${WORKDIR}/config/package-lists/gershwin.list.chroot config/package-lists/gershwin.list.chroot
-tar -xf ${WORKDIR}/system-${ARCH}.tar.gz -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/
-tar -xf ${WORKDIR}/developer-${ARCH}.tar.gz -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/
-tar -xf ${WORKDIR}/applications-${ARCH}.tar.gz -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/
+tar -xf ${WORKDIR}/system.txz -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/
+tar -xf ${WORKDIR}/applications.txz -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/
 ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/
 ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/Applications
 ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/Developer/Applications
 ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/System/Applications
-# Fetch and extract overlays
-wget -O opt.zip https://github.com/gershwin-os/opt/archive/refs/heads/main.zip
-wget -O etc.zip https://github.com/gershwin-os/etc/archive/refs/heads/main.zip
-wget -O desktop-pictures.zip https://github.com/gershwin-os/desktop-pictures/archive/refs/heads/main.zip
-mkdir -p ${WORKDIR}/live-default/config/includes.chroot_after_packages/etc
-mkdir -p ${WORKDIR}/live-default/config/includes.chroot_after_packages/opt
-mkdir -p ${WORKDIR}/live-default/config/includes.chroot_after_packages/System/Library/Desktop\ Pictures/
-bsdtar -xf opt.zip -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/opt/ --strip-components 1 --exclude 'LICENSE' --exclude 'README.md'
-bsdtar -xf etc.zip -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/etc/ --strip-components 1 --exclude 'LICENSE' --exclude 'README.md'
-bsdtar -xf desktop-pictures.zip -C ${WORKDIR}/live-default/config/includes.chroot_after_packages/System/Library/Desktop\ Pictures/ --strip-components 1 --exclude 'README.md'
-chmod +x ${WORKDIR}/live-default/config/includes.chroot_after_packages/opt/bin/*
-ls ${WORKDIR}/live-default/config/includes.chroot_after_packages/opt/
-
 
 cat <<EOF > config/hooks/live/gershwin.hook.chroot
 #!/bin/sh
